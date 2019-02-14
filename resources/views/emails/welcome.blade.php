266fb1969e8c9a9407e12220c3f44eeab6ce2019
timonefm <timonefm@gmail.com>
2019-02-14 11:41:27 +0600

    add all files to repository

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+<h2>Learning Laravel!</h2>
+
+<div>
+    Welcome to {{ $name }} website!
+</div>
+
+</body>
+</html>
+
